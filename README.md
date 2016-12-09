5fafd585041c62651faf078773c2564fc983b1be
Marco Grunert <marco.grunert@t-systems.com>
2016-12-09 14:22:21 +0100

    Solution

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # springboot-restdata-repoloading
 
-    https://github.com/magomi/springboot-restdata-repoloading.git
+Demo application related to http://stackoverflow.com/questions/41046736/how-to-exclude-a-repository-from-component-scan-when-using-spring-data-rest
+
+Run it: 
+
+    git clone https://github.com/magomi/springboot-restdata-repoloading.git
     mvn clean package
     java -jar target/api-0.0.1-SNAPSHOT.jar
--- api/src/main/java/org/codefromhell/test/repoloading/ApiApplication.java
+++ api/src/main/java/org/codefromhell/test/repoloading/ApiApplication.java
@@ -4,13 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-// exclude V02
+// exclude V03 (according to http://stackoverflow.com/questions/41046736/how-to-exclude-a-repository-from-component-scan-when-using-spring-data-rest)
 @SpringBootApplication
-@ComponentScan(excludeFilters = {
+@EnableJpaRepositories(excludeFilters = {
         @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {
                 DataRepository.class})
 })
+//// exclude V02
+//@SpringBootApplication
+//@ComponentScan(excludeFilters = {
+//        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {
+//                DataRepository.class})
+//})
 //// exclude V01
 //@SpringBootApplication(exclude = { DataRepository.class })
 public class ApiApplication {
